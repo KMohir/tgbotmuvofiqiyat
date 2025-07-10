1f0cd5648c02e6faeb40bcdd63810875833af4a0
KMohir <komilovmohirbekfda11@gmail.com>
2025-07-10 10:54:18 +0500

    fix .env 2

--- db.py
+++ db.py
@@ -33,7 +33,7 @@ class Database:
                 host=os.getenv('DB_HOST'),
                 dbname=os.getenv('DB_NAME'),
                 user=os.getenv('DB_USER'),
-                password=os.getenv('DB_PASSWORD'),
+                password=os.getenv('DB_PASS'),
                 port=os.getenv('DB_PORT', '5432')
             )
             self.conn.autocommit = True
